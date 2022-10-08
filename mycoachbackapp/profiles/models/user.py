070a3705011cc521fab803a3c10ba4553ff23753
maricm123 <mihailomaric001@gmail.com>
2022-10-08 20:54:39 +0200

    Added abstract user model class

---
+++ mycoachbackapp/profiles/models/user.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+
+class CustomUserManager(BaseUserManager):
+    """
+    Custom User Manager is required when redefining User class
+    """
+    def create_user(self,  email, password=None, **extra_fields):
+        email = self.normalize_email(email)
+
+        user = self.model(
+            email=email,
+            **extra_fields
+        )
+        user.set_password(
+            password
+        )
+
+        user.save()
+
+        return user
+
+    def create_superuser(self,  email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser has to have is_staff being true")
+
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser has to have is_superuser being true")
+
+        return self.create_user(email=email, password=password, **extra_fields)
+
+
+class User(
+    AbstractUser
+):
+    email = models.CharField(max_length=80, unique=True)
+    username = models.CharField(max_length=45)
+    
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"  # used as the unique identifier
+    REQUIRED_FIELDS = []  # a list of the field names that will be prompted with createsuperuser
+
+    def __str__(self):
+        return self.username
+
+    
+
